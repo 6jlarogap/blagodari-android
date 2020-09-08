69118a6a7ad06dba664428b06e34ebd92415c619
sergeGabrus <seg2007@mail.ru>
2020-09-08 11:58:05 +0300

    Уведомления через сообщения с данными.

--- app/src/main/java/blagodarie/rating/MyFirebaseMessagingServise.java
+++ app/src/main/java/blagodarie/rating/MyFirebaseMessagingServise.java
@@ -13,19 +13,10 @@ import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
-import com.google.firebase.iid.FirebaseInstanceIdReceiver;
-import com.google.firebase.iid.FirebaseInstanceIdService;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Random;
-
-import blagodarie.rating.ui.user.UserActivity;
 
 public class MyFirebaseMessagingServise
         extends FirebaseMessagingService {
@@ -56,104 +47,89 @@ public class MyFirebaseMessagingServise
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
 
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-                //scheduleJob();
-            } else {
-                // Handle message within 10 seconds
-                //handleNow();
+            String firstName = remoteMessage.getData().get("first_name");
+            String lastName = remoteMessage.getData().get("last_name");
+            String photo = remoteMessage.getData().get("photo");
+            String operationTypeIdString = remoteMessage.getData().get("operation_type_id");
+            int operationTypeId = 0;
+            if (operationTypeIdString != null) {
+                operationTypeId = Integer.parseInt(operationTypeIdString);
+            }
+            OperationType operationType = OperationType.getById(operationTypeId);
+            String comment = remoteMessage.getData().get("comment");
+
+            String title = null;
+            String channelId = null;
+            String channelName = null;
+            String channelDescription = null;
+            if (operationType != null) {
+                switch (operationType) {
+                    case THANKS:
+                        title = getApplicationContext().getString(R.string.notification_title_thanks_pattern, firstName, lastName);
+                        channelId = getApplicationContext().getString(R.string.notification_chanel_id_thanks);
+                        channelName = getApplicationContext().getString(R.string.notification_chanel_name_thanks);
+                        channelDescription = getApplicationContext().getString(R.string.notification_chanel_description_thanks);
+                        break;
+                    case MISTRUST:
+                        title = getApplicationContext().getString(R.string.notification_title_mistrus_pattern, firstName, lastName);
+                        channelId = getApplicationContext().getString(R.string.notification_chanel_id_mistrust);
+                        channelName = getApplicationContext().getString(R.string.notification_chanel_name_mistrust);
+                        channelDescription = getApplicationContext().getString(R.string.notification_chanel_description_mistrust);
+                        break;
+                    case MISTRUST_CANCEL:
+                        title = getApplicationContext().getString(R.string.notification_title_cancel_mistrust_pattern, firstName, lastName);
+                        channelId = getApplicationContext().getString(R.string.notification_chanel_id_cancel_mistrust);
+                        channelName = getApplicationContext().getString(R.string.notification_chanel_name_cancel_mistrust);
+                        channelDescription = getApplicationContext().getString(R.string.notification_chanel_description_cancel_mistrust);
+                        break;
+                    default:
+                        title = "";
+                }
             }
 
-        }
-
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-            if (remoteMessage.getNotification().getBody() != null) {
-                try {
-                    final JSONObject json = new JSONObject(remoteMessage.getNotification().getBody());
-                    String firstName = json.getString("first_name");
-                    String lastName = json.getString("last_name");
-                    String photo = json.getString("photo");
-                    int operationTypeId = json.getInt("operation_type_id");
-                    OperationType operationType = OperationType.getById(operationTypeId);
-                    String comment = json.getString("comment");
-
-                    String title = null;
-                    String channelId = null;
-                    String channelName = null;
-                    String channelDescription = null;
-                    if (operationType != null) {
-                        switch (operationType) {
-                            case THANKS:
-                                title = getApplicationContext().getString(R.string.notification_title_thanks_pattern, firstName, lastName);
-                                channelId = getApplicationContext().getString(R.string.notification_chanel_id_thanks);
-                                channelName = getApplicationContext().getString(R.string.notification_chanel_name_thanks);
-                                channelDescription = getApplicationContext().getString(R.string.notification_chanel_description_thanks);
-                                break;
-                            case MISTRUST:
-                                title = getApplicationContext().getString(R.string.notification_title_mistrus_pattern, firstName, lastName);
-                                channelId = getApplicationContext().getString(R.string.notification_chanel_id_mistrust);
-                                channelName = getApplicationContext().getString(R.string.notification_chanel_name_mistrust);
-                                channelDescription = getApplicationContext().getString(R.string.notification_chanel_description_mistrust);
-                                break;
-                            case MISTRUST_CANCEL:
-                                title = getApplicationContext().getString(R.string.notification_title_cancel_mistrust_pattern, firstName, lastName);
-                                channelId = getApplicationContext().getString(R.string.notification_chanel_id_cancel_mistrust);
-                                channelName = getApplicationContext().getString(R.string.notification_chanel_name_cancel_mistrust);
-                                channelDescription = getApplicationContext().getString(R.string.notification_chanel_description_cancel_mistrust);
-                                break;
-                            default:
-                                title = "";
-                        }
-                    }
-
-                    NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
-                            .setSmallIcon(R.drawable.ic_launcher)
-                            .setContentTitle(title)
-                            .setContentText(comment)
-                            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
+                    .setSmallIcon(R.drawable.ic_launcher)
+                    .setContentTitle(title)
+                    .setContentText(comment)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
 
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                        int importance = NotificationManager.IMPORTANCE_DEFAULT;
-                        NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
-                        channel.setDescription(channelDescription);
-                        // Register the channel with the system; you can't change the importance
-                        // or other notification behaviors after this
-                        NotificationManager notificationManager = getSystemService(NotificationManager.class);
-                        notificationManager.createNotificationChannel(channel);
-                    }
-                    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
+                channel.setDescription(channelDescription);
+                // Register the channel with the system; you can't change the importance
+                // or other notification behaviors after this
+                NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                notificationManager.createNotificationChannel(channel);
+            }
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
 
 // notificationId is a unique int for each notification that you must define
-                    //notificationManager.notify(1, builder.build());
-                    Handler uiHandler = new Handler(Looper.getMainLooper());
-                    uiHandler.post(() -> Picasso.get().load(photo)
-                            .resize(250, 250)
-                            .into(new Target() {
-                                @Override
-                                public void onBitmapLoaded (Bitmap bitmap, Picasso.LoadedFrom from) {
-                                    builder.setLargeIcon(bitmap);
-                                    notificationManager.notify(id++, builder.build());
-                                }
+            //notificationManager.notify(1, builder.build());
+            Handler uiHandler = new Handler(Looper.getMainLooper());
+            uiHandler.post(() -> Picasso.get().load(photo)
+                    .resize(250, 250)
+                    .into(new Target() {
+                        @Override
+                        public void onBitmapLoaded (Bitmap bitmap, Picasso.LoadedFrom from) {
+                            builder.setLargeIcon(bitmap);
+                            notificationManager.notify(id++, builder.build());
+                        }
 
-                                @Override
-                                public void onBitmapFailed (Exception e, Drawable errorDrawable) {
+                        @Override
+                        public void onBitmapFailed (Exception e, Drawable errorDrawable) {
 
-                                }
+                        }
 
-                                @Override
-                                public void onPrepareLoad (Drawable placeHolderDrawable) {
+                        @Override
+                        public void onPrepareLoad (Drawable placeHolderDrawable) {
 
-                                }
-                            }));
-                } catch (JSONException e) {
-                    Log.e(TAG, Log.getStackTraceString(e));
-                }
+                        }
+                    }));
 
-            }
         }
+
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
